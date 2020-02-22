8b76732397ac57fe70f9343bf978566a092a0e2e
Marta RoDi <rodriguezmdiaz@gmail.com>
2020-02-22 10:11:47 +0100

    service and organitation

---
+++ client/src/tools/userService.js
@@ -0,0 +1,36 @@
+import axios  from 'axios';
+
+class UserService {
+  constructor(){
+    this.service = axios.create({
+      baseURL: `${process.env.REACT_SERVER_UR}/users`
+    })
+  }
+
+  getAll = () => {
+    return this.service.get('/')
+    .then(response => response.data)
+  }
+
+  get = id => {
+    return this.service.get(`/user/${id}`)
+    .then(response => response.data)
+  }
+
+  create = (name, birthdate) => {
+    return this.service.post('/new', {name, birthdate})
+    .then(response => response.data)
+  }
+
+  update = (id, name, birthdate) => {
+    return this.service.post(`/edit/${id}`, {name, birthdate})
+    .then(response => response.data)
+  }
+
+  remove = id => {
+    return this.service.get(`/delete/${id}`)
+    .then(response => response.data)
+  }
+}
+
+export default UserService;
